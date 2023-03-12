b8752fa15f4ef9572e52b35eaefb610664fd4568
Rodrigo França <rfrancacosta@gmail.com>
2023-03-12 14:57:38 -0300

    Iniciando o projeto

---
+++ explorando-bean/src/main/java/br/com/franca/springboot/explorandobean/ExplorandoBeanApplication.java
@@ -0,0 +1,13 @@
+package br.com.franca.springboot.explorandobean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorandoBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorandoBeanApplication.class, args);
+	}
+
+}
---
+++ explorando-bean/src/test/java/br/com/franca/springboot/explorandobean/ExplorandoBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.franca.springboot.explorandobean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorandoBeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
